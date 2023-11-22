1947f2f0e603ffafd29f5201b4350c0e0e336805
kairat <jumanov7070@gmail.com>
2023-11-23 05:42:38 +0600

    slider is done

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -1,36 +1,42 @@
-import React from 'react'
-import sliderImg from '../assets/images/slider-img.png'
+import React, { useState } from 'react'
 import leftIcon from '../assets/icons/left.svg'
 import rightIcon from '../assets/icons/right.svg'
+import { Button } from './Button';
 
 
-const Slider = () => {
+
+const Slider = ({slides}) => {
+    const [currentIndex, SetCurrentIndex] = useState(0);
+
+    const toPrevious = () => {
+        let isFirstSLide = currentIndex === 0;
+        let newIndex = isFirstSLide ?slides.length-1 : currentIndex- 1;
+        SetCurrentIndex(newIndex)
+}
+
+    const toNext = () => {
+
+        let isLastSLide = currentIndex === slides.length-1
+        let newIndex = isLastSLide ? 0 : currentIndex + 1
+        SetCurrentIndex(newIndex)
+    }
   return (
-    <div className="relative w-[1377px] h-[508px] rounded-[48px]  mx-auto bg-[#D7D7D7]">
-      <img
-        src={sliderImg}
-        alt="Slider Image"
-        className="absolute right-0 top-[30%]   object-cover"
-      />
+   <div className=' relative rounded-[48px]  mx-8  justify-center bg-[#DDD]  h-[35rem]'>
 
-
-      <div className="absolute left-0 top-1/2 transform -translate-y-1/2 w-1/3 text-center">
-        <div className='ml-[118px]'>
-        <h1 className="text-4xl font-extrabold ">Подарки для неё и для него</h1>
-        <p className="text-lg">Покупай прямо сейчас</p>
-        <button className="border-[1px] border-solid  border-[#333]  text-[#333] px-4 py-2 mt-4 rounded-full">
-          Подробнее
-        </button>
-
-
-        </div>
-
-      </div>
-      <img src={leftIcon} className=' absolute  top-[50%]  left-[2%] cursor-pointer ' />
-      <img src={rightIcon} className=' absolute top-[50%]  right-[2%] cursor-pointer ' />
-
-
+    <img src={leftIcon} onClick={() => toPrevious()} className=' absolute top-1/2 transform -translate-y-1/2 left-[32px] cursor-pointer ' />
+    <div className='ml-[7rem] absolute top-1/2 transform -translate-y-1/2' >
+        <p className=' text-5xl font-extrabold text-[#333] max-w-[554px] mb-[1.2rem]'>{slides[currentIndex].title}</p>
+        <p className='text-[#333] text-[20px] mb-[2.4rem] '>{slides[currentIndex].subtitle}</p>
+        <Button className='rounded-[2rem] border-[2px] border-black border-solid flex justify-center items-center text-[18px] py-[0.8rem] px-[3rem] '  children={slides[currentIndex].btn} />
     </div>
+
+    <div>
+    <img src={slides[currentIndex].url} className='absolute right-0  top-1/2 transform -translate-y-1/2 w-[555px] h-[340px]' />
+    </div>
+    <img src={rightIcon} onClick={() => toNext()} className='absolute top-1/2 transform -translate-y-1/2 right-[32px] cursor-pointer'  />
+
+
+   </div>
   );
 };
 
--- src/pages/mainPage.js
+++ src/pages/mainPage.js
@@ -1,14 +1,23 @@
 import React from 'react'
-import { Button } from '../components/Button'
 import Banner from '../components/Banner'
 import Header from '../components/Header'
 import Categories from '../components/Categories'
 import Slider from '../components/Slider'
+import slider1 from '../assets/images/slider-img.png'
+
 
 
 
 
 export default function MainPage() {
+
+    const slides = [
+        { title: 'Подарки для неё и для него', subtitle : 'Покупай прямо сейчас', btn: 'Подробнее', url: slider1 },
+        { title: 'Выгодные акции', subtitle : 'Каждый день и круглый год!', btn: 'Подробнее', url: slider1 }
+
+    ]
+
+
   return (
     <div className=' w-[100vw] h-[100vh]'>
 
@@ -17,7 +26,7 @@ export default function MainPage() {
       <Banner/>
       <Header/>
       <Categories/>
-    <Slider/>
+      <Slider slides={slides}/>
       </div>
       
 
